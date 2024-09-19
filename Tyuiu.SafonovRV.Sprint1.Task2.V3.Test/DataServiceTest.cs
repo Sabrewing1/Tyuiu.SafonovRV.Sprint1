868844a2498cd56dc3311bee966db543ced4fdf8
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-09-19 20:51:40 +0500

    Task2

---
+++ Tyuiu.SafonovRV.Sprint1.Task2.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.SafonovRV.Sprint1.Task2.V3.Lib;
+
+namespace Tyuiu.SafonovRV.Sprint1.Task2.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        { 
+            DataService ds = new DataService();
+            int x = 1;
+            var res = ds.ConvertHourToMin(x);
+            Assert.AreEqual(60, res);
+        }
+    }
+}
---
+++ Tyuiu.SafonovRV.Sprint1.Task2.V3/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.SafonovRV.Sprint1.Task2.V3.Lib;
+namespace Tyuiu.SafonovRV.Sprint1.Task2.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService(); 
+            Console.Title = "Спринт #1 | Выполнил: Сафонов Р.В | АСОиУБ-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Сафонов Руслан Васильевич | АСОиУб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int x;
+            Console.WriteLine("Введите значение х в часах:                                                ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" Приобразованние значение часов x минутах  = " + ds.ConvertHourToMin(x));
+
+
+            Console.ReadLine();
+        }
+    }
+}
