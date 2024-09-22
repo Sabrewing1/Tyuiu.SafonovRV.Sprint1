aa3e23a85c2db045a58be3030e3a022f4f707fa2
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-09-22 22:06:47 +0500

    Task5

--- Tyuiu.SafonovRV.Sprint1.Task5.V2.Lib/DataService.cs
+++ Tyuiu.SafonovRV.Sprint1.Task5.V2.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.SafonovRV.Sprint1.Task5.V2.Lib
             double c = 9;
 
 
-            return (int) Math.Round(c/(a * ( temp - b) ) );
+            return (int) (0.555556*(temp - b));
         }
     }
 }
