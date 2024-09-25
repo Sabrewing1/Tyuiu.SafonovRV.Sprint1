e29fe93327955b0bc6837e3274211f88d269337f
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-09-25 17:34:10 +0500

    Task7

--- Tyuiu.SafonovRV.Sprint1.Task7.V9.Test/DataServiceTest.cs
+++ Tyuiu.SafonovRV.Sprint1.Task7.V9.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.SafonovRV.Sprint1.Task7.V9.Test
             DataService ds = new DataService();
             double x = 2;
             double y = 4;
-            double z = 5.982;
+            double z = 5.983;
             var res = ds.Calculate(x, y);
             Assert.AreEqual(z, res);
             
